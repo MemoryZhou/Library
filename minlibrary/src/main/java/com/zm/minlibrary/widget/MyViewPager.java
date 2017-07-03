4971ae2c354c77f4a20f8d753585cc6597b38db5
zm <553419781@qq.com>
2017-07-03 16:44:20 +0800

    viewpager

---
+++ minlibrary/src/main/java/com/zm/minlibrary/widget/MyViewPager.java
@@ -0,0 +1,56 @@
+package com.zm.minlibrary.widget;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by zm on 2017/7/3.
+ */
+
+public class MyViewPager extends ViewPager {
+
+    public MyViewPager(Context context) {
+        super(context);
+    }
+
+    public MyViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+//  @Override
+//  public boolean onInterceptTouchEvent(MotionEvent arg0) {
+//      return true;
+//  }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                if (listener != null) {
+                    listener.onTouchDown();
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                if (listener != null) {
+                    listener.onTouchUp();
+                }
+                break;
+            default:
+                break;
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+    private OnViewPagerTouchEvent listener;
+
+    public void setOnViewPagerTouchEventListener(OnViewPagerTouchEvent l){
+        listener = l;
+    }
+
+    public interface OnViewPagerTouchEvent{
+        void onTouchDown();
+        void onTouchUp();
+    }
+}
